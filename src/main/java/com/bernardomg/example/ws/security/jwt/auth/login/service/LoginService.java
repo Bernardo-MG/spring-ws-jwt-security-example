2f97658c0731d384682614088588f8ace34e0fb5
Bernardo-MG <programming@bernardomg.com>
2022-09-20 16:24:01 +0200

    Fixed package names

---
+++ src/main/java/com/bernardomg/example/ws/security/jwt/auth/login/service/LoginService.java
@@ -0,0 +1,8 @@
+
+package com.bernardomg.example.ws.security.jwt.auth.login.service;
+
+public interface LoginService {
+
+    public String login(final String username, final String password);
+
+}
--- src/main/java/com/bernardomg/example/ws/security/jwt/resource/auth/login/service/LoginService.java
+++
@@ -1,8 +0,0 @@
-
-package com.bernardomg.example.ws.security.jwt.resource.auth.login.service;
-
-public interface LoginService {
-
-    public String login(final String username, final String password);
-
-}
