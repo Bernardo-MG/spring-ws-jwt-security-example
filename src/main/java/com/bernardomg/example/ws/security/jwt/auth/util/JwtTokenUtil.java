6e34acf93827fdf9915a668b4f08677a23114ced
Bernardo <programming@bernardomg.com>
2022-09-22 08:53:52 +0200

    Updated JWT procedure

--- src/main/java/com/bernardomg/example/ws/security/jwt/auth/util/JwtTokenUtil.java
+++ src/main/java/com/bernardomg/example/ws/security/jwt/auth/util/JwtTokenUtil.java
@@ -7,6 +7,8 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.function.Function;
 
+import javax.crypto.SecretKey;
+
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Component;
@@ -14,6 +16,7 @@ import org.springframework.stereotype.Component;
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
 
 @Component
 public class JwtTokenUtil implements Serializable {
@@ -21,6 +24,8 @@ public class JwtTokenUtil implements Serializable {
     public static final long  JWT_TOKEN_VALIDITY = 5 * 60 * 60;
 
     private static final long serialVersionUID   = -2550185165626007488L;
+
+    private final SecretKey key = Keys.secretKeyFor(SignatureAlgorithm.HS512);
 
     @Value("${jwt.secret}")
     private String            secret;
@@ -59,20 +64,20 @@ public class JwtTokenUtil implements Serializable {
     // Serialization(https://tools.ietf.org/html/draft-ietf-jose-json-web-signature-41#section-3.1)
     // compaction of the JWT to a URL-safe string
     private String doGenerateToken(final Map<String, Object> claims, final String subject) {
-
         return Jwts.builder()
             .setClaims(claims)
             .setSubject(subject)
             .setIssuedAt(new Date(System.currentTimeMillis()))
             .setExpiration(new Date(System.currentTimeMillis() + (JWT_TOKEN_VALIDITY * 1000)))
-            .signWith(SignatureAlgorithm.HS512, secret)
+            .signWith(key, SignatureAlgorithm.HS512)
             .compact();
     }
 
     // for retrieveing any information from token we will need the secret key
     private Claims getAllClaimsFromToken(final String token) {
-        return Jwts.parser()
-            .setSigningKey(secret)
+        return Jwts.parserBuilder()
+            .setSigningKey(key)
+            .build()
             .parseClaimsJws(token)
             .getBody();
     }
