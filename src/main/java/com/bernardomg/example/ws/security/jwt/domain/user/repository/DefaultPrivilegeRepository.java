443a23ae203fee05e8d0ef6ae553082f31df492b
Bernardo <programming@bernardomg.com>
2022-09-29 18:37:54 +0200

    Fixed privileges repository

--- src/main/java/com/bernardomg/example/ws/security/jwt/domain/user/repository/DefaultPrivilegeRepository.java
+++ src/main/java/com/bernardomg/example/ws/security/jwt/domain/user/repository/DefaultPrivilegeRepository.java
@@ -1,3 +1,26 @@
+/**
+ * The MIT License (MIT)
+ * <p>
+ * Copyright (c) 2022 the original author or authors.
+ * <p>
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * <p>
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ * <p>
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
 
 package com.bernardomg.example.ws.security.jwt.domain.user.repository;
 
@@ -30,7 +53,7 @@ public final class DefaultPrivilegeRepository implements PrivilegeRepository {
     /**
      * Query for finding the privileges of a user.
      */
-    private final String                     queryForUser = "SELECT p.name FROM privileges p LEFT JOIN role_privileges rp ON p.id = rp.role_id LEFT JOIN roles r ON r.id = rp.role_id LEFT JOIN USER_ROLES ur ON r.id = ur.role_id LEFT JOIN users u ON u.id = ur.user_id WHERE u.id = :id";
+    private final String                     queryForUser = "SELECT p.name AS name FROM privileges p JOIN role_privileges rp ON p.id = rp.privilege_id JOIN roles r ON r.id = rp.role_id JOIN USER_ROLES ur ON r.id = ur.role_id JOIN users u ON u.id = ur.user_id WHERE u.id = :id";
 
     @Override
     public final Collection<Privilege> findForUser(final Long id) {
--- src/test/java/com/bernardomg/example/ws/security/jwt/test/domain/user/repository/integration/ITPrivilegeRepository.java
+++ src/test/java/com/bernardomg/example/ws/security/jwt/test/domain/user/repository/integration/ITPrivilegeRepository.java
@@ -1,6 +1,9 @@
 
 package com.bernardomg.example.ws.security.jwt.test.domain.user.repository.integration;
 
+import java.util.Collection;
+import java.util.stream.Collectors;
+
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -14,7 +17,7 @@ import com.bernardomg.example.ws.security.jwt.test.config.annotation.Integration
 import liquibase.repackaged.org.apache.commons.collections4.IterableUtils;
 
 @IntegrationTest
-@DisplayName("Fee year repository - find all for year - full year")
+@DisplayName("Privilege repository")
 @Sql({ "/db/queries/user/single.sql", "/db/queries/role/single.sql", "/db/queries/privilege/multiple.sql",
         "/db/queries/relationship/role_privilege.sql", "/db/queries/relationship/user_role.sql" })
 public class ITPrivilegeRepository {
@@ -27,13 +30,39 @@ public class ITPrivilegeRepository {
     }
 
     @Test
-    @DisplayName("Returns all the entities")
-    public void testGetAll_Count() {
+    @DisplayName("Returns all the privileges for a user")
+    public void testFindForUser_Count() {
         final Iterable<? extends Privilege> result;
 
         result = repository.findForUser(1L);
 
         Assertions.assertEquals(3, IterableUtils.size(result));
+    }
+
+    @Test
+    @DisplayName("Returns all the data for the privileges of a user")
+    public void testFindForUser_Data() {
+        final Collection<? extends Privilege> result;
+        final Collection<String>              privileges;
+
+        result = repository.findForUser(1L);
+        privileges = result.stream()
+            .map(Privilege::getName)
+            .collect(Collectors.toList());
+
+        Assertions.assertTrue(privileges.contains("CREATE_USER"));
+        Assertions.assertTrue(privileges.contains("READ_USER"));
+        Assertions.assertTrue(privileges.contains("UPDATE_USER"));
+    }
+
+    @Test
+    @DisplayName("Returns no privileges for a not existing user")
+    public void testFindForUser_notExisting() {
+        final Iterable<? extends Privilege> result;
+
+        result = repository.findForUser(-1L);
+
+        Assertions.assertEquals(0, IterableUtils.size(result));
     }
 
 }
