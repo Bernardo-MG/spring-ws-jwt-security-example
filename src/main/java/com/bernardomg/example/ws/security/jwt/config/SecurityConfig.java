7d60973f2c309f55fd7ed3e1f33c4ff76c44944b
Bernardo <programming@bernardomg.com>
2022-10-01 13:33:39 +0200

    Reduced number of config classes

--- src/main/java/com/bernardomg/example/ws/security/jwt/config/SecurityConfig.java
+++ src/main/java/com/bernardomg/example/ws/security/jwt/config/SecurityConfig.java
@@ -26,6 +26,7 @@ package com.bernardomg.example.ws.security.jwt.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -43,6 +44,7 @@ import com.bernardomg.example.ws.security.jwt.auth.userdetails.PersistentUserDet
  *
  */
 @Configuration
+@EnableGlobalMethodSecurity(prePostEnabled = true, securedEnabled = true, jsr250Enabled = true)
 public class SecurityConfig {
 
     public SecurityConfig() {
