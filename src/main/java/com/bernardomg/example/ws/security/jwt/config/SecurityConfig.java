bbb8b92652d7675af6840d852216906de75549a0
Bernardo-MG <programming@bernardo-mg.com>
2023-02-06 09:41:44 +0100

    Merged configuration classes

--- src/main/java/com/bernardomg/example/ws/security/jwt/config/SecurityConfig.java
+++ src/main/java/com/bernardomg/example/ws/security/jwt/config/SecurityConfig.java
@@ -28,6 +28,7 @@ import java.nio.charset.Charset;
 
 import javax.crypto.SecretKey;
 
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
@@ -55,6 +56,7 @@ import io.jsonwebtoken.security.Keys;
  */
 @Configuration
 @EnableGlobalMethodSecurity(prePostEnabled = true, securedEnabled = true, jsr250Enabled = true)
+@EnableConfigurationProperties(JwtProperties.class)
 public class SecurityConfig {
 
     public SecurityConfig() {
