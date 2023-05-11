7bff18cbc13403cadf0753da6b9ba9f612c6b0ca
Bernardo Martinez <bmartinez@hiberus.com>
2023-05-11 13:27:35 +0200

    Added javadoc

--- src/main/java/com/bernardomg/example/spring/security/ws/jwt/security/jwt/token/ImmutableJwtTokenData.java
+++ src/main/java/com/bernardomg/example/spring/security/ws/jwt/security/jwt/token/ImmutableJwtTokenData.java
@@ -1,3 +1,26 @@
+/**
+ * The MIT License (MIT)
+ * <p>
+ * Copyright (c) 2023 the original author or authors.
+ * <p>
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * <p>
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ * <p>
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
 
 package com.bernardomg.example.spring.security.ws.jwt.security.jwt.token;
 
@@ -6,16 +29,34 @@ import java.util.Date;
 import lombok.Builder;
 import lombok.Value;
 
+/**
+ * Immutable implementation of the JWT token data.
+ * 
+ * @author Bernardo Mart&iacute;nez Garrido
+ *
+ */
 @Value
 @Builder(setterPrefix = "with")
 public class ImmutableJwtTokenData implements JwtTokenData {
 
+    /**
+     * Expiration date.
+     */
     private final Date   expiration;
 
+    /**
+     * Issued at date.
+     */
     private final Date   issuedAt;
 
+    /**
+     * Subject.
+     */
     private final String subject;
 
+    /**
+     * Validity date.
+     */
     private final Date   validity;
 
 }
--- src/main/java/com/bernardomg/example/spring/security/ws/jwt/security/jwt/token/JwtTokenData.java
+++ src/main/java/com/bernardomg/example/spring/security/ws/jwt/security/jwt/token/JwtTokenData.java
@@ -1,16 +1,61 @@
+/**
+ * The MIT License (MIT)
+ * <p>
+ * Copyright (c) 2023 the original author or authors.
+ * <p>
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * <p>
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ * <p>
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
 
 package com.bernardomg.example.spring.security.ws.jwt.security.jwt.token;
 
 import java.util.Date;
 
+/**
+ * Represents the commons JWT token claims.
+ * 
+ * @author Bernardo Mart&iacute;nez Garrido
+ *
+ */
 public interface JwtTokenData {
 
+    /**
+     * Returns the expiration date.
+     * @return the expiration date
+     */
     public Date getExpiration();
 
+    /**
+     * Returns the issued at date.
+     * @return the issued at date
+     */
     public Date getIssuedAt();
 
+    /**
+     * Returns the subject.
+     * @return the subject
+     */
     public String getSubject();
 
+    /**
+     * Returns the validity date.
+     * @return the validity date
+     */
     public Date getValidity();
 
 }
