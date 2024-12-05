b607bb47fddb10552b85014e2899720a05b1a62b
Bernardo <programming@bernardomg.com>
2024-12-05 20:15:54 +0100

    Updated project

--- src/main/java/com/bernardomg/example/spring/security/ws/jwt/security/entrypoint/ErrorResponseAccessDeniedHandler.java
+++ src/main/java/com/bernardomg/example/spring/security/ws/jwt/security/entrypoint/ErrorResponseAccessDeniedHandler.java
@@ -26,13 +26,12 @@ package com.bernardomg.example.spring.security.ws.jwt.security.entrypoint;
 
 import java.io.IOException;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.security.access.AccessDeniedException;
 import org.springframework.security.web.access.AccessDeniedHandler;
 
-import com.bernardomg.example.spring.security.ws.jwt.mvc.error.model.Error;
-import com.bernardomg.example.spring.security.ws.jwt.mvc.response.model.ErrorResponse;
-import com.bernardomg.example.spring.security.ws.jwt.mvc.response.model.Response;
+import com.bernardomg.web.response.domain.model.ErrorResponse;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 import jakarta.servlet.ServletException;
@@ -60,7 +59,6 @@ public final class ErrorResponseAccessDeniedHandler implements AccessDeniedHandl
     public final void handle(final HttpServletRequest request, final HttpServletResponse response,
             final AccessDeniedException accessDeniedException) throws IOException, ServletException {
         final ErrorResponse resp;
-        final Error         error;
         final ObjectMapper  mapper;
 
         log.debug("Access denied failure for path {}: {}", request.getServletPath(),
@@ -69,8 +67,7 @@ public final class ErrorResponseAccessDeniedHandler implements AccessDeniedHandl
         response.setContentType(MediaType.APPLICATION_JSON_VALUE);
         response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
 
-        error = Error.of("Unauthorized");
-        resp = Response.error(error);
+        resp = new ErrorResponse(String.valueOf(HttpStatus.UNAUTHORIZED.value()), "Unauthorized");
 
         mapper = new ObjectMapper();
         mapper.writeValue(response.getOutputStream(), resp);
